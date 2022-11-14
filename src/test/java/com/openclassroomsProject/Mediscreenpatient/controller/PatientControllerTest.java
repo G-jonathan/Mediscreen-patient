0e360bebe2b548aa5e75306e7dd5cdaced9fa2ae
jonathan.G <jonathan.gouveia@protonmail.com>
2022-11-14 18:16:58 +0100

    first_commit

---
+++ src/test/java/com/openclassroomsProject/Mediscreenpatient/controller/PatientControllerTest.java
@@ -0,0 +1,37 @@
+package com.openclassroomsProject.Mediscreenpatient.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PatientControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPatientById() {
+    }
+
+    @Test
+    void getAllPatient() {
+    }
+
+    @Test
+    void addPatient() {
+    }
+
+    @Test
+    void updatePatient() {
+    }
+
+    @Test
+    void deletePatient() {
+    }
+}
